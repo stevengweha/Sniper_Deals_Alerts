a0f9ba421c4ddb657e866d582305ebb4b41fcc13
stevengweha <ngwehasteve@gmail.com>
2026-05-20 00:14:39 +0200

    security: apply new .gitignore and purge tracked data files

--- dbt_project/target/compiled/smart_buy/models/shopping/staging/stg_ebay_data.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    timestamp,
-    'ebay' AS source,
-    title,
-    CAST(REPLACE(REGEXP_REPLACE(price_raw, '[^0-9\.,]', '', 'g'), ',', '.') AS FLOAT) AS price,
-    NULL AS url
-FROM "smart_buy_db"."public"."raw_ebay"
--- dbt_project/target/compiled/smart_buy/models/staging/stg_leboncoin.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    timestamp,
-    'leboncoin' AS source,
-    title,
-    -- Extraction du prix numérique pour Postgres
-    CAST(NULLIF(REGEXP_REPLACE(price_raw, '[^0-9]', '', 'g'), '') AS FLOAT) AS price,
-    url
-FROM "smart_buy_db"."public"."raw_leboncoin"
